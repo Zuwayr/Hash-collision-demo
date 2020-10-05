75243ddb2bb4775910efc96dccd7230422b105a9
Zuwayr <50750289+Zuwayr@users.noreply.github.com>
2020-10-05 13:31:33 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+const md5File = require('md5-file')
+ 
+const hashOne = md5File.sync('message1.bin')
+const hashTwo = md5File.sync('message2.bin')
+
+console.log(`The MD5 sum of message1.bin is: ${hashOne}`)
+console.log(`The MD5 sum of message2.bin is: ${hashTwo}`)
